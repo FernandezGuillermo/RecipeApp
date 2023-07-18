e817bfc60c7575932206e46ea577f41d13dd4ce7
Fernandez Guillermo <fernandezguillermo86@gmail.com>
2023-07-18 07:56:28 -0300

    working in recipes

--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,8 +1,34 @@
+require('../models/database');
+const category = require('../models/category.js')
+
 /**
  * GET /
  * Homepage
  */
 
 exports.homepage = async(req,res) =>{
-    res.render('index', {title: 'Homepage'});
+    try {
+        const limitNumber = 5;
+        const categories = await category.find({}).limit(limitNumber);
+        res.render('index', {title: 'Cooking Blog - Home',categories});
+    } catch (error) {
+        res.status(500).send({message:error.message || "Error Ocured"})
+    }
 }
+
+/**
+ * GET / categories
+ * Categories
+ */
+
+exports.exploreCategories = async(req,res) =>{
+    try {
+        const limitNumber = 20;
+        const categories = await category.find({}).limit(limitNumber);
+        res.render('categories', {title: 'Cooking Blog - Categories',categories});
+    } catch (error) {
+        res.status(500).send({message:error.message || "Error Ocured"})
+    }
+}
+
+
